cc374d95ada0b6bd237ea373df14a390190e2756
Nicholas Marshall <nmarshall23@gmail.com>
2012-03-30 18:33:51 -0400

    Plugin now loads in Craftbukkit.

---
+++ src/main/scala/com/gmail/nmarshall23/demo/DemoPlugin.scala
@@ -0,0 +1,16 @@
+package com.gmail.nmarshall23.demo
+
+import org.bukkit.plugin.PluginManager
+import org.bukkit.plugin.java.JavaPlugin
+
+class DemoPlugin extends JavaPlugin {
+
+	override def onEnable : Unit = {
+	  
+	}
+	
+	override def onDisable : Unit = {
+	  
+	}
+	  
+}
