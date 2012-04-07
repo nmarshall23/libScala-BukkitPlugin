3f5ce600bde67f2d272940946a000cb52673b69e
Nicholas Marshall <nmarshall23@gmail.com>
2012-04-07 10:51:57 -0400

    Build now, finds Scala library and includes it into the jar

--- src/main/scala/com/gmail/nmarshall23/demo/DemoPlugin.scala
+++ src/main/scala/com/gmail/nmarshall23/demo/DemoPlugin.scala
@@ -5,14 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin
 import java.util.logging.Logger
 
 class DemoPlugin extends JavaPlugin {
-  var log: Logger = null
   
   override def onEnable : Unit = {
-      log = Logger.getLogger("Minecraft")
-	  val pm = this.getServer().getPluginManager()
-	  pm.registerEvents(new ListenerHandler(this), this)
-	  
-	  //log.info("Scala Enabled!")
   }
 	
   override def onDisable : Unit = {
